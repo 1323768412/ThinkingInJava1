fb58f88936a29c1a3908b65fee69170fb4d9cab9
wangz <wangz@2>
2020-05-13 12:27:56 +0800

    new

---
+++ src/exceptions/NewExceptions.java
@@ -0,0 +1,29 @@
+package exceptions;
+
+public class NewExceptions extends Exception {
+
+	public NewExceptions() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public NewExceptions(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NewExceptions(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NewExceptions(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NewExceptions(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
